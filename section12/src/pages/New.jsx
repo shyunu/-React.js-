eb0da3ee9ba09df585579506265dbc5fd11358b7
suhyun-kim9 <ksh020674@gmail.com>
2024-12-04 18:57:00 +0900

    감정일기장완성 및 배포

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,5 +1,28 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import { useEffect } from "react";
+import usePageTitle from "../hooks/usePageTitle";
+
 const New = () => {
-  return <div>New</div>;
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+  usePageTitle("새 일기 쓰기");
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true }); //뒤로가기 방지 : replace(true)
+  };
+
+  return (
+    <div>
+      <Header title={"새 일기 쓰기"} leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />} />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
